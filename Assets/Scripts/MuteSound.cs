ab07e0722fc38544ae949434c473d7d2dce22650
NorthyPark <32385215+NorthyPark@users.noreply.github.com>
2018-02-13 16:14:01 +0100

    stuff done

---
+++ Assets/Scripts/MuteSound.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MuteSound : MonoBehaviour
+{
+    AudioSource audioSource;
+    public bool alreadyTriggered;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (!alreadyTriggered)
+        {
+            audioSource.mute = !audioSource.mute;
+            alreadyTriggered = true;
+        }
+    }
+}
---
+++ Assets/characterController1.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class characterController1 : MonoBehaviour
+{
+
+    private Vector3 velocity = Vector3.zero;
+    public float speed = 10.0F;
+
+    private Rigidbody rb;
+
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+		rb = GetComponent<Rigidbody> (); 
+    }
+
+    void FixedUpdate()
+    {
+        PerformMovement();
+
+        float translation = Input.GetAxis("Vertical") * speed;
+        float straffe = Input.GetAxis("Horizontal") * speed;
+        translation *= Time.deltaTime;
+        straffe *= Time.deltaTime;
+
+        transform.Translate(straffe, 0, translation);
+
+        if (Input.GetKeyDown("escape"))
+            Cursor.lockState = CursorLockMode.None;
+    }
+
+    void PerformMovement()
+    {
+        if (velocity != Vector3.zero)
+        {
+            rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime);              //"MovePosition different from transform.Translate because this will stop the Rigidbody from moving there if it collides with something on the way. Easier to control than the AddForce method
+        }
+    }
+}
