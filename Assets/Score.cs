df6e8a8f310080ef0afa8255454186d7c3a68654
NorthyPark <32385215+NorthyPark@users.noreply.github.com>
2018-02-13 17:06:47 +0100

    schtuff v2

---
+++ Assets/Score.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class Score : MonoBehaviour {
+
+    public int score;
+
+    void OnTriggerEnter(Collider other)
+    {
+        PlayerPrefs.SetInt("Score", PlayerPrefs.GetInt("Score") + score);
+            Destroy(gameObject);
+    }
+
+}
