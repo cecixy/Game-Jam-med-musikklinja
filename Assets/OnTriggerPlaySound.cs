364019aa510dc9823f5a1a277a2a1340b78558c9
cecixy <cecibasket12@hotmail.com>
2018-02-16 14:09:56 +0100

    last sht

---
+++ Assets/OnTriggerPlaySound.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnTriggerPlaySound : MonoBehaviour {
+
+
+	private AudioSource audi; 
+
+
+	void Start()
+	{
+		audi = GetComponent<AudioSource> (); 
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		FindObjectOfType<AudioManager> ().Play ("PickUpTB"); 
+	}
+
+}
