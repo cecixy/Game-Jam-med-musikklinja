10aa0fe8dc8f944911445c220e85e433429a1cc6
cecixy <cecibasket12@hotmail.com>
2018-02-18 17:54:56 +0100

    added sound from google drive

---
+++ Assets/Scripts/BadMusicTrigger.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BadMusicTrigger : MonoBehaviour {
+
+
+
+	void OnTriggerEnter (Collider other)
+	{
+		GetComponent<AudioSource> ().Play (); 
+	}
+
+	void OnTriggerExit (Collider other)
+	{
+		GetComponent<AudioSource> ().Pause (); 
+	}
+
+}
